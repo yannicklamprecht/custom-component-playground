42bda2ad92aad98b36c0a3d7d184748e2aef8f38
Yannick Lamprecht <yannick.lamprecht@7p-group.com>
2024-11-01 21:57:19 +0100

    extend vanilla component with fragment script tag copying

--- vanilla/includeComponent.js
+++ vanilla/includeComponent.js
@@ -1,13 +1,31 @@
 export function define() {
+    const parser = new DOMParser();
     window.customElements.define("include-fragment", class extends HTMLElement {
         connectedCallback() {
             const src = this.getAttribute("src");
+            const tag = this.getAttribute("tag");
             fetch(src)
                 .then(response => response.text())
-                .then(text => {
-                    const template = document.createElement("v-container");
-                    template.innerHTML = text;
+                .then(text => parser.parseFromString(text, "text/html"))
+                .then(htmlUnsafe => {
+                    const template = document.createElement(tag ?? "div");
+                    this._copyScript(htmlUnsafe);
+                    template.innerHTML = htmlUnsafe.body.innerHTML;
                     this.replaceWith(template)
+                });
+        }
+
+        _copyScript(htmlUnsafe) {
+            htmlUnsafe.querySelectorAll("script")
+                .forEach((oldScript) => {
+                    const newScript = document.createElement("script");
+                    Array.from(oldScript.attributes).forEach((attr) => {
+                        newScript.setAttribute(attr.name, attr.value);
+                    });
+                    if (oldScript.textContent) {
+                        newScript.textContent = oldScript.textContent;
+                    }
+                    document.body.appendChild(newScript);
                 });
         }
     });
